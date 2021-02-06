e11785b0bd8013a8afccd3cde3c054e4e575c386
root-344 <michimasa8@gmail.com>
2021-02-07 04:46:45 +0900

    新規登録ユーザー情報修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -63,37 +63,7 @@ RSpec.describe User, type: :model do
         @user.valid?
         expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
       end
-      it 'ユーザー本名は、名字と名前がそれぞれ必須であること' do
-        @user.last_name = ''
-        @user.first_name = ''
-        @user.valid?
-        expect(@user.errors.full_messages).to include('Password is invalid', "Last name can't be blank",
-                                                      "First name can't be blank")
-      end
-      it 'ユーザー本名は、全角（漢字・ひらがな・カタカナ）での入力が必須であること' do
-        @user.last_name = 'aaaa'
-        @user.first_name = 'bbbb'
-        @user.valid?
-        expect(@user.errors.full_messages).to include('Password is invalid')
-      end
-      it 'ユーザー本名のフリガナは、名字と名前がそれぞれ必須であること' do
-        @user.last_name_kata = ''
-        @user.first_name_kata = ''
-        @user.valid?
-        expect(@user.errors.full_messages).to include('Password is invalid', "Last name kata can't be blank",
-                                                      "First name kata can't be blank")
-      end
-      it 'ユーザー本名のフリガナは、全角（カタカナ）での入力が必須であること' do
-        @user.last_name_kata = 'aiueo'
-        @user.first_name_kata = 'kakikukeko'
-        @user.valid?
-        expect(@user.errors.full_messages).to include('Password is invalid')
-      end
-      it '生年月日が必須であること' do
-        @user.birth = ''
-        @user.valid?
-        expect(@user.errors.full_messages).to include('Password is invalid', "Birth can't be blank")
-      end
+      
     end
   end
 end
