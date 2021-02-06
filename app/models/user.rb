cea81ac9178d06f54e5993d35f67565013f018cf
root-344 <michimasa8@gmail.com>
2021-02-07 04:37:06 +0900

    rubocop修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,13 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/
-  validates :password, presence: true, format: {with: VALID_PASSWORD_REGEX}
+
   validates :nickname, presence: true
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/
+  validates :password, presence: true, format: { with: VALID_PASSWORD_REGEX }
   validates :last_name, presence: true
   validates :first_name, presence: true
   validates :last_name_kata, presence: true
   validates :first_name_kata, presence: true
   validates :birth, presence: true
-
 end
