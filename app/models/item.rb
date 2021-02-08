81c9f2626143d5d90875c030e7077d7c698dca2e
root-344 <michimasa8@gmail.com>
2021-02-09 00:28:55 +0900

    アクティブハッシュ

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,14 +1,19 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
+  belongs_to :product_states
+  belongs_to :shippong_charges
+  belongs_to :region_id
+  belongs_to :eta_id
+  
   with_options presence: true do
     validates :product
     validates :category_id, numericality: { other_than: 1 }
     validates :product_description
-    validates :product_states_id
-    validates :shippong_charges_id
-    validates :region_id
-    validates :eta_id
+    validates :product_states_id, numericality: { other_than: 1 }
+    validates :shippong_charges_id, numericality: { other_than: 1 }
+    validates :region_id, numericality: { other_than: 1 }
+    validates :eta_id, numericality: { other_than: 1 }
     validates :price
   end
   belongs_to :user
