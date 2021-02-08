9494360252857fec8b12f78ac0864e9d3b8f9c9e
root-344 <michimasa8@gmail.com>
2021-02-08 15:03:20 +0900

    バリデーション作成

---
+++ app/models/item.rb
@@ -0,0 +1,13 @@
+class Item < ApplicationRecord
+  with_options presence: true do
+    validates :product
+    validates :class_id
+    validates :product_description
+    validates :product_states_id
+    validates :shippong_charges_id
+    validates :region_id
+    validates :eta_id
+    validates :price
+  end
+  belongs_to :user
+end
