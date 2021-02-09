03617aa385f9f3b006878f667ef7bde054a87c61
root-344 <michimasa8@gmail.com>
2021-02-09 12:15:10 +0900

    アソシエーション変更

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,20 +2,20 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :product_states
-  belongs_to :shippong_charges
-  belongs_to :region_id
-  belongs_to :eta_id
-  
+  belongs_to :shipping_charges
+  belongs_to :region
+  belongs_to :eta
+  belongs_to :user
+  has_one_attached :images
+
   with_options presence: true do
     validates :product
     validates :category_id, numericality: { other_than: 1 }
     validates :product_description
     validates :product_states_id, numericality: { other_than: 1 }
-    validates :shippong_charges_id, numericality: { other_than: 1 }
+    validates :shipping_charges_id, numericality: { other_than: 1 }
     validates :region_id, numericality: { other_than: 1 }
     validates :eta_id, numericality: { other_than: 1 }
     validates :price
   end
-  belongs_to :user
-  has_one_attached :images
 end
