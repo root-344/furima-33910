9fdbc90d2afd5aed7cb17c0c6b55dbf90ae54f98
root-344 <michimasa8@gmail.com>
2021-02-10 20:18:19 +0900

    console.log削除

--- app/javascript/price.js
+++ app/javascript/price.js
@@ -4,10 +4,8 @@ window.addEventListener("load",()=>{
   priceInput.addEventListener("input", () => {
     const inputValue = priceInput.value;
 
-
     const addTaxDom = document.getElementById("add-tax-price")
     addTaxDom.innerHTML = (Math.floor(inputValue * 0.1));
-     console.log(addTaxDom);
 
      const profitNumber = document.getElementById("profit")
      const value_result = inputValue * 0.1
