c335bd94156f90d87c4fd5e7b56fe9e549c21ccd
root-344 <michimasa8@gmail.com>
2021-02-12 00:58:07 +0900

    商品編集機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -22,6 +22,22 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+    unless @item.user_id == current_user.id
+      redirect_to action: :index
+    end
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path
+    else 
+      render :edit
+    end
+  end
+
   private
   def item_params
     params.require(:item).permit(:product, :category_id, :product_description, :product_states_id, :shipping_charges_id, :region_id, :eta_id, :price, :image).merge(user_id: current_user.id)
