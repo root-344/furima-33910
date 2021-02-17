a27f12483de89d4d0652030f26e1419d8c4d582e
root-344 <michimasa8@gmail.com>
2021-02-17 17:37:31 +0900

    itemsコントローラー訂正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -49,7 +49,7 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    unless @item.user_id == current_user.id || @item.purchase == nil
+    if @item.user_id != current_user.id || @item.purchase.present?
       redirect_to action: :index
     end 
   end
